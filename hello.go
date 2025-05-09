f1b09a34279495fd4bab5119977bd85589c60f40
VladKolmorgan <kolmorganvlad@gmail.com>
2025-05-09 16:28:01 -0600

    Added hello.go

---
+++ hello.go
@@ -0,0 +1,15 @@
+import (
+	"fmt"
+	"os"
+	//"github.com/go-openapi/strfmt"
+)
+
+func main() {
+
+	for idx, arg := range os.Args {
+		fmt.Println(idx, arg)
+	}
+
+	//a := strfmt.IsEmail("vlad@test.com")
+	//fmt.Println(a)
+}
